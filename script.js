a0508794386c097e30074878d88ffc963e2e6bc5
unknown <himanshu9518@gmail.com>
2021-04-23 11:49:51 +0530

    Ready for work✨

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fn1(){
     var str = document.getElementById("text1").value;
     var str2 = '+-"not+verified"+-"unverified"+-"needed"+-"required"&f=live'
-    window.open("https://twitter.com/search?q=verified+"+str+str2)
+    var str3 = '';
+    
+
+    var markedCheckbox = document.getElementsByName("check");  
+    for (var checkbox of markedCheckbox) {  
+    if (checkbox.checked) 
+      str3 = str3.concat(checkbox.value+'+OR+');  
+    }
+    window.open("https://twitter.com/search?q=verified+"+str+"+"+str3+"plasma"+str2);
 }
